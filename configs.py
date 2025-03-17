735edca8a28ed20c39434401b4b030602b695a99
Nimesh Akalanka <90953455+nimeshakalanka@users.noreply.github.com>
2025-03-17 22:23:25 +0530

    ✨✨✨✨

--- configs.py
+++ configs.py
@@ -1,15 +1,9 @@
 from os import path, getenv
 
 class Config:
-    API_ID = 23241556  #your API ID
-    API_HASH = "959e240fdfbff381f179212037fb861f"  #your API HASH
-    BOT_TOKEN = "6176824033:AAHuxe3Plqb6pvCaJ_RhbxvMLGVtgC8WEz4"  #BOT TOKEN
-    CHANNEL_ID = -1001749057548
-    MONGO_URI = "mongodb+srv://admin:admin@cluster0.tljjz3n.mongodb.net/?retryWrites=true&w=majority"
-    SUDO = [1853733618,
-            435328284]
-    #U1 = 123456
-    #u2 = 456789
-    #U1_CHID = -100xxxxxxxx
-    #U2_CHID = -100xxxxxxxx
+    API_ID = 123456  #API ID
+    API_HASH = "959e240fdffghjt381f2874674fgdsfgb861f"  #API HASH
+    BOT_TOKEN = "610003200:AAmagaHgyiyurfJ_RhbxvMLGVtgC8WEz4"  #BOT TOKEN
+    MONGO_URI = "mongodb+srv://tik:tok@cluster0.tiktok.mongodb.net/?retryWrites=true&w=majority" #mongodb url
+    SUDO = [123456789] #admins ids
 cfg = Config()
