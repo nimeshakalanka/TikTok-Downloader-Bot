5adcac06d0b4a9957b80d84b2ca612ee79e6b22b
Nimesh Akalanka <90953455+Mafia58@users.noreply.github.com>
2025-03-17 19:25:08 +0530

    ✨

---
+++ configs.py
@@ -0,0 +1,15 @@
+from os import path, getenv
+
+class Config:
+    API_ID = 23241556  #your API ID
+    API_HASH = "959e240fdfbff381f179212037fb861f"  #your API HASH
+    BOT_TOKEN = "6176824033:AAHuxe3Plqb6pvCaJ_RhbxvMLGVtgC8WEz4"  #BOT TOKEN
+    CHANNEL_ID = -1001749057548
+    MONGO_URI = "mongodb+srv://admin:admin@cluster0.tljjz3n.mongodb.net/?retryWrites=true&w=majority"
+    SUDO = [1853733618,
+            435328284]
+    #U1 = 123456
+    #u2 = 456789
+    #U1_CHID = -100xxxxxxxx
+    #U2_CHID = -100xxxxxxxx
+cfg = Config()
